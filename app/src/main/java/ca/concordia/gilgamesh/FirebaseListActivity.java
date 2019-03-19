319c9f7176dcba966b2964bd7669fe08853ca44a
agrek <agrek@users.noreply.github.com>
2019-03-19 18:14:35 -0400

    Created Fragments and button to add each separate User, Location, and Machine

--- app/src/main/java/ca/concordia/gilgamesh/FirebaseListActivity.java
+++ app/src/main/java/ca/concordia/gilgamesh/FirebaseListActivity.java
@@ -2,12 +2,16 @@ package ca.concordia.gilgamesh;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -58,16 +62,88 @@ public class FirebaseListActivity extends BaseActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = findViewById(R.id.container);
         mViewPager.setAdapter(mPagerAdapter);
-        TabLayout tabLayout = findViewById(R.id.tabs);
+        TabLayout tabLayout = findViewById(R.id.firebase_list_tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
         // Button launches NewPostActivity
-        //findViewById(R.id.fabNewPost).setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View v) {
-        //        startActivity(new Intent(FirebaseListActivity.this, NewPostActivity.class));
-        //    }
-        //});
+        findViewById(R.id.fabNewPost).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FirebaseListActivity.this, NewUserActivity.class));
+            }
+        });
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            FloatingActionButton floatingActionButton = findViewById(R.id.fabNewPost);
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                final int position = tab.getPosition();
+
+                // Toast.makeText(getApplicationContext(), "TabSelected" + String.valueOf(tab.getPosition()), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "TabSelected" + String.valueOf(position));
+
+                switch (position) {
+                    case 0:
+                        Log.d(TAG, "Case 0" + String.valueOf(position));
+                        findViewById(R.id.fabNewPost).setOnClickListener(null);
+                        // Button launches NewPostActivity
+                        findViewById(R.id.fabNewPost).setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                startActivity(new Intent(FirebaseListActivity.this, NewUserActivity.class));
+                                Log.d(TAG, "OnCLick" + String.valueOf(position));
+                            }
+                        });
+                        break;
+                    case 1:
+                        Log.d(TAG, "Case 1" + String.valueOf(position));
+                        findViewById(R.id.fabNewPost).setOnClickListener(null);
+                        // Button launches NewPostActivity
+                        findViewById(R.id.fabNewPost).setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                startActivity(new Intent(FirebaseListActivity.this, NewLocationActivity.class));
+                                Log.d(TAG, "OnCLick" + String.valueOf(position));
+                            }
+                        });
+                        break;
+                    case 2:
+                        Log.d(TAG, "Case 2" + String.valueOf(position));
+                        findViewById(R.id.fabNewPost).setOnClickListener(null);
+                        // Button launches NewPostActivity
+                        findViewById(R.id.fabNewPost).setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                startActivity(new Intent(FirebaseListActivity.this, NewMachineActivity.class));
+                                Log.d(TAG, "OnCLick" + String.valueOf(position));
+                            }
+                        });
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+                int position = tab.getPosition();
+
+                // Toast.makeText(getApplicationContext(), "TabUnselected" + String.valueOf(tab.getPosition()), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+                int position = tab.getPosition();
+
+                // Toast.makeText(getApplicationContext(), "TabReselected" + String.valueOf(tab.getPosition()), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
     @Override
